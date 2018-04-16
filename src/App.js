c5603df15482d172778e9e787a296e177751adb7
Pascal Fecht <pfecht@inovex.de>
2018-04-16 15:57:53 +1000

    chore(): Integrate inovex-elements and migrate todo app from stencil-in-angular

--- src/App.js
+++ src/App.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+export class Todo {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      newTodoName: 'Initial value',
+      todos: [
+        new Todo('TODO 1'),
+        new Todo('TODO 2'),
+        new Todo('TODO 3')
+      ]
+    };
+  }
+
+  onValueChanged(value) {
+    this.setState({newTodoName: value});
+  }
+
+  add() {
+    this.state.todos.push(new Todo(this.state.newTodoName));
+    this.setState({newTodoName: ''});
+  }
+
+  delete(index, todo) {
+    const todos = this.state.todos.filter(currentTodo => currentTodo !== todo);
+    this.setState({todos: todos});
+  }
+
+
+  todoActionTemplate() {
+    return (
+      <div className="action-box">
+        <ino-input 
+          type="text" 
+          value={this.state.newTodoName} 
+          onKeyUp={(ev) => this.onValueChanged(ev.target.value)} 
+          placeholder="What needs to be done..."></ino-input>
+        <ino-button onClick={(ev) => this.add(ev)} icon-right="question">Add</ino-button>
+      </div>
+    );
+  }
+
+
+  todoListTemplate() {
+    return (
+      <ul className="todos">
+        { this.state.todos.map((todo, index) => {
+            return <li key={index}>
+              {todo.name} 
+              <ino-button onClick={() => this.delete(index, todo)}>Delete</ino-button>
+            </li>;
+          })
+        }
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Simple TODO</h2>
+
+        <div className="app-wrapper">
+          {this.todoActionTemplate()}
+          {this.todoListTemplate()}
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'stencil-components/dist/stencil-components';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
